a7850b5a5f199698b74a0ce6baa667c7f1e8b0a4
Kayan Lira <kayan.lira@m4u.com.br>
2022-02-01 21:37:49 -0300

    ?

--- src/main/kotlin/br/com/boasaude/msassociados/infrastructure/Extensions.kt
+++ src/main/kotlin/br/com/boasaude/msassociados/infrastructure/Extensions.kt
@@ -1,10 +1,11 @@
 package br.com.boasaude.msassociados.infrastructure
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun String.toLocalDate(): LocalDate {
     return try {
-        LocalDate.parse(this)
+        LocalDate.parse(this, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
     } catch (ex: Exception) {
         LocalDate.now()
     }
