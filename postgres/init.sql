9f7e6fa7dfd2b8cd49688cf1742a8cd709d4c049
Kayan Lira <kayan.lira@m4u.com.br>
2022-02-11 09:55:45 -0300

    schema documentos criado

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,3 +2,6 @@ CREATE SCHEMA "associados";
 GRANT ALL PRIVILEGES ON SCHEMA "associados" TO "postgres";
 GRANT ALL PRIVILEGES ON all tables in schema "associados" TO "postgres";
 
+CREATE SCHEMA "documentos";
+GRANT ALL PRIVILEGES ON SCHEMA "documentos" TO "postgres";
+GRANT ALL PRIVILEGES ON all tables in schema "documentos" TO "postgres";
